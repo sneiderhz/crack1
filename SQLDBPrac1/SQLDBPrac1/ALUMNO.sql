e167731c32807be04d8c025167441cc62e6724c8
elker <sneiderherrera.sh@gmail.com>
2021-09-03 09:48:26 -0500

    Creacion de tablas

---
+++ SQLDBPrac1/SQLDBPrac1/ALUMNO.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[ALUMNO]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Nombre] NVARCHAR(50) NOT NULL, 
+    [Ciudad] NVARCHAR(50) NOT NULL, 
+    [Edad] INT NOT NULL
+)
---
+++ SQLDBPrac1/SQLDBPrac1/CURSO.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[CURSO]
+(
+	[Cod] INT NOT NULL PRIMARY KEY, 
+    [Nombre ] NVARCHAR(50) NOT NULL, 
+    [Fecha_inicio] NVARCHAR(50) NOT NULL, 
+    [Duracion] INT NOT NULL, 
+    [Valor] INT NOT NULL
+)
