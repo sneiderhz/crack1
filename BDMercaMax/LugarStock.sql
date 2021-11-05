530eba5c6747ad47ccd20157c87405f94731816a
elker <sneiderherrera.sh@gmail.com>
2021-11-05 11:48:36 -0500

    BDMercaMax

---
+++ BDMercaMax/LugarStock.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[LugarStock]
+(
+	[id_lugar] INT NOT NULL PRIMARY KEY, 
+    [barcode_prodcuto] INT NOT NULL UNIQUE, 
+    [cantidad_bodega] INT NOT NULL, 
+    [cantidad_gondola] INT NOT NULL, 
+    [seccion_bodega] NVARCHAR(MAX) NOT NULL, 
+    [seccion_gondola] NVARCHAR(MAX) NOT NULL
+)
---
+++ BDMercaMax/Producto.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Producto]
+(
+	[id_producto] INT NOT NULL PRIMARY KEY, 
+    [nombre_producto] VARCHAR(MAX) NOT NULL, 
+    [fecha_llegada] DATE NOT NULL, 
+    [fecha_nacimiento] DATE NOT NULL, 
+    [barcode] INT NOT NULL, 
+    [precio] DECIMAL(18, 2) NOT NULL, 
+    [nit] INT NOT NULL, 
+    [id_tipo] INT NOT NULL
+)
